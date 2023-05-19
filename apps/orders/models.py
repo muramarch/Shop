223ab761d70a0c5e7a8f84e39fb6f448f136ccbc
Mura Aitkulov <muramarch@MacBook-Air-Mura.local>
2023-05-19 15:26:22 +0600

    add order apps functional

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from apps.shops.models import Product
 
-# Create your models here.
+User = get_user_model()
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Добавьте другие поля, если необходимо
